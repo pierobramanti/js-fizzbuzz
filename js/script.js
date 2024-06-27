e01ce3c6aaa439d17a797e950a175123ef44c0a3
pierobramanti <piero.bramanti94@gmail.com>
2024-06-27 16:02:41 +0200

    [UPD] inserted last condition and finished exercise

--- js/script.js
+++ js/script.js
@@ -10,7 +10,11 @@ else if (i % 3 === 0){
     console.log("Fizz")
 }
 // INSERISCO LA CONDIZIONE DIVISIBILE PER 5// 
-else if( i % 5===0){
+else if( i % 5 === 0){
     console.log("Buzz")
+}
+// INSERISCO LA CONDIZIONE DIVISIBILE PER I RESTANTI NUMERI// 
+else {
+    console.log(`${i}`)
 }
 }
