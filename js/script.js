e7c0a66c62c83cab2492c4dd047019f9421acb0c
pierobramanti <piero.bramanti94@gmail.com>
2024-06-27 15:57:59 +0200

    [UPD] inserted second conditions

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ for (let i = 1; i <= 100; i++){
 // INSERISCO LA CONDIZIONE DIVISIBILE PER 3 e PER 5//
 if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz")
-
+// INSERISCO LA CONDIZIONE DIVISIBILE PER 3//
 }
+else if (i % 3 === 0){
+    console.log("Fizz")
+}
+   
 }
