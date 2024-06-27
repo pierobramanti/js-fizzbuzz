19e5f747d237cd80952ecfd907e633ef06ad10f8
pierobramanti <piero.bramanti94@gmail.com>
2024-06-27 15:55:09 +0200

    [UPD] inserted first condition

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 // INSERISCO CICLO FOR//
 for (let i = 1; i <= 100; i++){
-    
+
+// INSERISCO LA CONDIZIONE DIVISIBILE PER 3 e PER 5//
+if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz")
+
+}
 }
